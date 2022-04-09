4db2726116602aad1cb761eb888ad1eb2f27ef66
alepekhin <alepekhine@gmail.com>
2022-04-09 15:18:19 +0300

    just after spring initializr

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # api-gateway
+
+API gateway microservice with Spring Cloud Contract Verifier
+
+## Иницифлизация
+
+Проект построен с помощью https://start.spring.io/
+
+
---
+++ src/test/java/alepekhin/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package alepekhin.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
