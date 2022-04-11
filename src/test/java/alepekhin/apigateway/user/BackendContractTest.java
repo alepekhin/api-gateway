44e82a496f60af594958955a0992acabba1e40de
alepekhin <alepekhine@gmail.com>
2022-04-11 19:29:36 +0300

    backend contract test added

--- src/test/java/alepekhin/apigateway/user/BackendContractTest.java
+++ src/test/java/alepekhin/apigateway/user/BackendContractTest.java
@@ -6,21 +6,27 @@ import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+@AutoConfigureStubRunner(
+        stubsMode = StubRunnerProperties.StubsMode.CLASSPATH,
+        ids = "5334b05c-f5c8-48a5-adc3-922fdb7533fc")
 @ActiveProfiles("test")
-public class WireMockTest {
+public class BackendContractTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -35,20 +41,13 @@ public class WireMockTest {
     }
 
     @Test
-    public void testStub() throws Exception {
-        System.out.println("inside testStub");
-
-
-        stubFor(WireMock.get(urlMatching("/users")) // called on wiremock server localhost:7070
-                .willReturn(aResponse().withHeader("Content-TYpe", "application/json")
-                        .withStatus(200).withBodyFile("users.json"))); // file inside resources/__files
-
+    public void testBackendContract() throws Exception {
+        System.out.println("inside testBackendContract");
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/users") // called on controller
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andReturn();
 
         System.out.println("stub returned "+ result.getResponse().getContentAsString());
-
     }
 }
